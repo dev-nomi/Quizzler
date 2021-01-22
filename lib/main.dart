ffa1622e1cb480be3c576f13bc9e5e0a52dc6f38
noman-khalid-dev <noman.khalid.freelance@gmail.com>
2021-01-22 18:10:01 +0500

    create main structure of app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(Quizzler());
 }
+
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          title: Text(
+            'Quizzler',
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
   }
 }
 
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: RaisedButton(
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                onPressed: () {
+                  print('hello');
+                }),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: RaisedButton(
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                onPressed: () {
+                  print('hello');
+                }),
+          ),
+        ),
+      ],
+    );
+  }
+}
